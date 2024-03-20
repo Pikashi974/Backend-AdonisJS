5b4b71cb1e58b8baa1caa9129829685e2a14d77b
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2024-03-20 10:34:56 +0400

    patch profile image

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,5 +1,7 @@
 import User from '#models/user'
+import { cuid } from '@adonisjs/core/helpers'
 import { HttpContext } from '@adonisjs/core/http'
+import { unlinkSync } from 'node:fs'
 
 export default class UsersController {
   async profile({ auth, response }: HttpContext) {
@@ -26,6 +28,31 @@ export default class UsersController {
         area: area ? area : user.area,
         tel: tel ? tel : user.tel,
         job: job ? job : user.job,
+      })
+      .save()
+    const { password, enabled, isAdmin, ...userInfo } = user.$attributes
+    return response.ok(userInfo)
+  }
+  async changeImage({ auth, request, response }: HttpContext) {
+    const user = await auth.authenticate()
+
+    const img = request.file('img', {
+      extnames: ['jpg', 'png', 'jpeg'],
+      size: '4mb',
+    })
+
+    //Check if the image is valid
+    if (!img || !img.isValid) {
+      return response.badRequest({ message: 'Invalid image' })
+    }
+
+    const filename = `${cuid()}.${img.extname}`
+    await img.move('public/uploads', { name: filename })
+    unlinkSync(`public/uploads/${user.img}`)
+
+    await user
+      .merge({
+        img: filename ? filename : user.img,
       })
       .save()
     const { password, enabled, isAdmin, ...userInfo } = user.$attributes
--- start/routes.ts
+++ start/routes.ts
@@ -21,5 +21,6 @@ router
     router.get('/profile', '#controllers/users_controller.profile')
     router.get('/users', '#controllers/users_controller.list')
     router.patch('/profile', '#controllers/users_controller.patchProfile')
+    router.patch('/profile/img', '#controllers/users_controller.changeImage')
   })
   .use(middleware.auth({ guards: ['api'] }))
